1f5f53154ecfbd4c6d299e93b71ad553a5ba44a5
AdrianaMonterroza <adrianacesde@gmail.com>
2025-11-13 11:54:49 -0500

    feat se envia la solución del primer punto

---
+++ AdrianaMonterroza/script.js
@@ -0,0 +1,11 @@
+// Programa que calcula el total de una compra en el supermercado
+
+let producto1 = Number(prompt("Ingresa el precio del primer producto:"));
+let producto2 = Number(prompt("Ingresa el precio del segundo producto:"));
+let producto3 = Number(prompt("Ingresa el precio del tercer producto:"));
+
+let total = producto1 + producto2 + producto3;
+
+console.log("El total a pagar es: $" + total);
+
+
