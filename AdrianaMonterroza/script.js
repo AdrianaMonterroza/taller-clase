faeeeeb62c4f3a1a9753098a023ea8c734b0a470
AdrianaMonterroza <adrianacesde@gmail.com>
2025-11-13 12:33:44 -0500

    feat se envia solución del cuarto punto de la actividad

--- AdrianaMonterroza/script.js
+++ AdrianaMonterroza/script.js
@@ -1,5 +1,4 @@
 // //Punto uno
-
 // // Programa que calcula el total de una compra en el supermercado
 
 // let producto1 = Number(prompt("Ingresa el precio del primer producto:"));
@@ -11,7 +10,6 @@
 // console.log("El total a pagar es: $" + total);
 
 //Punto dos
-
 // //Para un evento, necesito un banner. Pide el **ancho y el alto** de una pared (en metros) y calcula su **área** para saber cuánto material comprar.
 
 // let ancho = Number(prompt("Ingresa el ancho de la pared en metros:"));
@@ -22,15 +20,21 @@
 // console.log("El área del banner es: " + area + " metros cuadrados.");
 
 //Tercer punto 
-
 //Debo calcular cuánto perímetro tiene un potrero rectangular. Pide el largo y el ancho del potrero y muestra el resultado.
 
-let largo = Number(prompt("Ingrese el largo del potrero en centimetros:"));
-let ancho = Number(prompt("Ingrese el ancho del potrero en centimetros:"));
+// let largo = Number(prompt("Ingrese el largo del potrero en centimetros:"));
+// let ancho = Number(prompt("Ingrese el ancho del potrero en centimetros:"));
 
-let perimetro = largo * ancho; 
+// let perimetro = largo * ancho; 
 
-console.log ("El perimetro del potrero es: " + perimetro + "centimetros")
+// console.log ("El perimetro del potrero es: " + perimetro + "centimetros")
+
+//Cuarto punto
+// Mi app del clima solo muestra la temperatura en grados Fahrenheit. Pide la temperatura en °F y conviértela a grados Celsius.
+
+let Fahrenheit = Number(prompt("Ingresa la temperatura en grados Fahrenheit (°F):"));
+celsius = (Fahrenheit - 32) * 5/9;
+console.log("La temperatura en grados Celsius es: " + celsius + " °C");
 
 
 
