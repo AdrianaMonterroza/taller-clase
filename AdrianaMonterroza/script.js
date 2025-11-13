2f4961554998431144557161262a9821eb3fc3b9
AdrianaMonterroza <adrianacesde@gmail.com>
2025-11-13 12:23:55 -0500

    feat se envia solución del punto tres de la actividad

--- AdrianaMonterroza/script.js
+++ AdrianaMonterroza/script.js
@@ -1,13 +1,36 @@
+// //Punto uno
+
+// // Programa que calcula el total de una compra en el supermercado
+
+// let producto1 = Number(prompt("Ingresa el precio del primer producto:"));
+// let producto2 = Number(prompt("Ingresa el precio del segundo producto:"));
+// let producto3 = Number(prompt("Ingresa el precio del tercer producto:"));
+
+// let total = producto1 + producto2 + producto3;
+
+// console.log("El total a pagar es: $" + total);
+
 //Punto dos
 
-//Para un evento, necesito un banner. Pide el **ancho y el alto** de una pared (en metros) y calcula su **área** para saber cuánto material comprar.
+// //Para un evento, necesito un banner. Pide el **ancho y el alto** de una pared (en metros) y calcula su **área** para saber cuánto material comprar.
 
-let ancho = Number(prompt("Ingresa el ancho de la pared en metros:"));
-let alto = Number(prompt("Ingresa el alto de la pared en metros:"));
+// let ancho = Number(prompt("Ingresa el ancho de la pared en metros:"));
+// let alto = Number(prompt("Ingresa el alto de la pared en metros:"));
 
-let area = ancho * alto;
+// let area = ancho * alto;
 
-console.log("El área del banner es: " + area + " metros cuadrados.");
+// console.log("El área del banner es: " + area + " metros cuadrados.");
+
+//Tercer punto 
+
+//Debo calcular cuánto perímetro tiene un potrero rectangular. Pide el largo y el ancho del potrero y muestra el resultado.
+
+let largo = Number(prompt("Ingrese el largo del potrero en centimetros:"));
+let ancho = Number(prompt("Ingrese el ancho del potrero en centimetros:"));
+
+let perimetro = largo * ancho; 
+
+console.log ("El perimetro del potrero es: " + perimetro + "centimetros")
 
 
 
